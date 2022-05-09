c581966a8035c863e76d2e45160caed75d5fe843
Harshad Nawathe <harshadn@thoughtworks.com>
2022-05-09 19:38:05 +0530

    Add DataComponentProcessor

---
+++ plugins/datamesh-backend/src/processor/DataComponentProcessor.ts
@@ -0,0 +1,78 @@
+import {Entity, parseEntityRef, getCompoundEntityRef, ComponentEntity} from '@backstage/catalog-model';
+import {
+    CatalogProcessor,
+    LocationSpec,
+    CatalogProcessorEmit,
+    processingResult
+} from '@backstage/plugin-catalog-backend';
+
+
+export interface DataComponentEntity extends ComponentEntity {
+    spec : ComponentEntity["spec"] & {
+        consumesResources?: string[],
+        producesResources?: string[],
+    }
+}
+
+export class DataComponentProcessor implements CatalogProcessor {
+    getProcessorName = (): string => {
+        return "DataComponentProcessor";
+    };
+
+    postProcessEntity = async (entity: Entity, _location: LocationSpec, emit: CatalogProcessorEmit): Promise<Entity> => {
+        const selfRef = getCompoundEntityRef(entity);
+
+        function doEmit(
+            targets: string | string[] | undefined,
+            context: { defaultKind?: string; defaultNamespace: string },
+            outgoingRelation: string,
+            incomingRelation: string,
+        ): void {
+            if (!targets) {
+                return;
+            }
+            for (const target of [targets].flat()) {
+                const targetRef = parseEntityRef(target, context);
+                emit(
+                    processingResult.relation({
+                        source: selfRef,
+                        type: outgoingRelation,
+                        target: {
+                            kind: targetRef.kind,
+                            namespace: targetRef.namespace,
+                            name: targetRef.name,
+                        },
+                    }),
+                );
+                emit(
+                    processingResult.relation({
+                        source: {
+                            kind: targetRef.kind,
+                            namespace: targetRef.namespace,
+                            name: targetRef.name,
+                        },
+                        type: incomingRelation,
+                        target: selfRef,
+                    }),
+                );
+            }
+        }
+
+
+
+        if (entity.kind === "Component" ) {
+            const component = entity as ComponentEntity;
+
+            if(component.spec.type === "data") {
+                const dataComponent = component as DataComponentEntity
+                doEmit(
+                    dataComponent.spec.consumesResources,
+                    { defaultKind: 'Resource', defaultNamespace: selfRef.namespace },
+                    'consumesResource',
+                    'resourceConsumedBy',
+                );
+            }
+        }
+        return entity;
+    };
+}
